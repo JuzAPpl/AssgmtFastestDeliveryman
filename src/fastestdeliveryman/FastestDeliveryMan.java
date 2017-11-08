abb6b2b31a5e4766b139cccc05f542039255b4e4
Leo <Leo@Leo>
2017-11-08 19:54:16 +0800

    test

---
+++ src/fastestdeliveryman/FastestDeliveryMan.java
@@ -0,0 +1,19 @@
+
+package fastestdeliveryman;
+
+import java.util.*;
+
+/**
+ *
+ * @author Leo
+ */
+public class FastestDeliveryMan {
+    public static void registerAffiliate(){
+        Scanner reader = new Scanner(System.in);
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
