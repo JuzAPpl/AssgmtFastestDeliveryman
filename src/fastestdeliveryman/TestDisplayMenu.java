ea5a0b33613bc6baa9bc1b4749a18952de55dc09
JuzAPpl <32742178+JuzAPpl@users.noreply.github.com>
2017-11-27 20:41:30 +0800

    Modified addFood(Food) in Menu to make it keep track of the length Added search for food feature in Customer(now using dummy data)

--- src/fastestdeliveryman/TestDisplayMenu.java
+++ src/fastestdeliveryman/TestDisplayMenu.java
@@ -15,16 +15,28 @@ public class TestDisplayMenu {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Food food1 = new Food(1, "Delicious Kuey Teow", 4.99, 5, Food.FOOD_UNAVAILABLE);
-        Food food2 = new Food(2, "East DonDong Fried Chicken", 9.99, 7.6, Food.FOOD_AVAILABLE);
-        Food food3 = new Food(3, "Super Mega Jumbo Burger", 8.79, 10, Food.FOOD_PROMOTION);
-        
-        Food[] foodArr = {food1, food2, food3};
-        Menu menu = new Menu(foodArr);
-        
-        Affiliate affiliate = new Affiliate("East DonDong", "Mac DonDong's", "PV13", "011-2334567", menu);
-        
-        affiliate.getMenu().showMenu();
+//        Food food1 = new Food(1, "Delicious Kuey Teow", 4.99, 5, Food.FOOD_UNAVAILABLE);
+//        Food food2 = new Food(2, "East DonDong Fried Chicken", 9.99, 7.6, Food.FOOD_AVAILABLE);
+//        Food food3 = new Food(3, "Super Mega Jumbo Burger", 8.79, 10, Food.FOOD_PROMOTION);
+//        
+//        Food[] foodArr0 = {food1, food2, food3};
+//        Menu menu0 = new Menu(foodArr0);
+//        
+//        Affiliate affiliate0 = new Affiliate("East DonDong", "Mac DonDong's", "PV13", "011-2334567", menu0);
+//        
+//        
+//        Food food4 = new Food(1, "Very Delicious Kuey Teow", 4.99, 5, Food.FOOD_UNAVAILABLE);
+//        Food food5 = new Food(2, "West DonDong Fried Chicken", 9.99, 7.6, Food.FOOD_AVAILABLE);
+//        Food food6 = new Food(3, "Super Mega Giga Jumbo Burger", 8.79, 10, Food.FOOD_PROMOTION);
+//        
+//        Food[] foodArr1 = {food4, food5, food6};
+//        Menu menu1 = new Menu(foodArr1);
+//        
+//        Affiliate affiliate1 = new Affiliate("West DonDong", "Mac DinDing's", "PV13", "011-2334567", menu1);
+//        
+//        Affiliate[] affiliates ={affiliate0, affiliate1}; 
+//            affiliate0.getMenu().showMenu();
+
     }
     
 }
