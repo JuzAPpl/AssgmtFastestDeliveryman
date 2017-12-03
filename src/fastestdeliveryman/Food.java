42c6020506bb7a773368b8fbf4fac081df1faa15
LimFangChun <leo477831@gmail.com>
2017-12-03 19:42:41 +0800

    Partially implementing linked list Fixed some bugs

--- src/fastestdeliveryman/Food.java
+++ src/fastestdeliveryman/Food.java
@@ -6,6 +6,7 @@
 package fastestdeliveryman;
 
 import java.util.Scanner;
+import ADT.*;
 
 /**
  *
@@ -13,11 +14,10 @@ import java.util.Scanner;
  */
 
 public class Food implements FoodInterface {
-    
+
     public static final int FOOD_UNAVAILABLE = 0;
     public static final int FOOD_AVAILABLE = 1;
     public static final int FOOD_PROMOTION = 2;
-    
 
     private int ID;
     private String name;
@@ -25,24 +25,25 @@ public class Food implements FoodInterface {
     private double preparationTime;
 
     private int status; //0: unavailable, 1: available, 2: promotion
-    
-    public Food(){
+
+    public Food() {
         this(-1, "", -1.0, -1.0, 0);
     }
-    
-    public Food(int ID, String name, double price, double preparationTime, int status){
-        this.ID=ID;
-        this.name=name;
-        this.price=price;
-        this.preparationTime=preparationTime;
-        this.status=status;
+
+    public Food(int ID, String name, double price, double preparationTime, int status) {
+        this.ID = ID;
+        this.name = name;
+        this.price = price;
+        this.preparationTime = preparationTime;
+        this.status = status;
     }
 
     //setters and getters
-    public int getID(){
+    @Override
+    public int getID() {
         return ID;
     }
-    
+
     @Override
     public void setName(String name) {
         this.name = name;
@@ -73,9 +74,39 @@ public class Food implements FoodInterface {
         return String.format("%.1f", preparationTime);
     }
 
+    @Override
+    public String toString() {
+        return String.format("%-5d %20s %9.2f %17s %10s\n", ID, name, price, (getPreparationTime() + " min"), getStatus());
+    }
+
+    @Override
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    @Override
+    public String getStatus() {
+        String status;
+        switch (this.status) {
+            case FOOD_UNAVAILABLE:
+                status = "Unavailable";
+                break;
+            case FOOD_AVAILABLE:
+                status = "Available";
+                break;
+            case FOOD_PROMOTION:
+                status = "Promotion";
+                break;
+            default:
+                status = "";
+                break;
+        }
+        return status;
+    }
+    
     public static int getNewFoodStatus() {
         Scanner reader = new Scanner(System.in);
-        int newStatus ;
+        int newStatus;
         int choiceOfStatus;
         Boolean validInput;
 
@@ -89,7 +120,15 @@ public class Food implements FoodInterface {
             System.out.println("========================================");
             System.out.print("Your choice: ");
             choiceOfStatus = Integer.parseInt(reader.nextLine());
+
             validInput = choiceOfStatus == -1 || (choiceOfStatus >= 1 && choiceOfStatus <= 3);
+
+            if (!validInput) {
+                System.out.println("===================");
+                System.out.println("Invalid choice");
+                System.out.println("Please try again");
+                System.out.println("===================");
+            }
         } while (!validInput);
 
         switch (choiceOfStatus) {
@@ -106,32 +145,5 @@ public class Food implements FoodInterface {
                 newStatus = -1;
         }
         return newStatus;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("%-5d %20s %9.2f %17s %10d\n", ID, name, price, (getPreparationTime() + " min"), status);
-    
-    public void setStatus(int status){
-        this.status=status;
-    }
-    
-    public String getStatus(){
-        String status;
-        switch(this.status){
-            case FOOD_UNAVAILABLE:
-                status = "Unavailable";
-                break;
-            case FOOD_AVAILABLE:
-                status="Available";
-                break;
-            case FOOD_PROMOTION:
-                status="Promotion";
-                break;
-            default:
-                status = "";
-                break;
-        }
-        return status;
     }
 }
