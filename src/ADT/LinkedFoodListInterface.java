37da7f8852bc802f8df286588d8a70c0a3e717a9
LimFangChun <leo477831@gmail.com>
2017-12-09 13:13:40 +0800

    Privileged for Food class only

---
+++ src/ADT/LinkedFoodListInterface.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ADT;
+
+/**
+ *
+ * @author Leo
+ * @param <T>
+ */
+public interface LinkedFoodListInterface<T> {
+    public void add(T newEntry);
+    public boolean add(T newEntry, int newPosition);
+    public T remove(int givenPosition);
+    public void clear();
+    public boolean replace(int givenPosition, T newEntry);
+    public T getEntry(int givenPosition);
+    public boolean contains(T entry);
+    public int getNumberOfEntries();
+    public boolean isEmpty();
+    public void displayMenuItemWithStatusOrder();
+}
