d0f9702003ab04ac55564b689b380fa3de90a2f3
LimFangChun <leo477831@gmail.com>
2017-12-09 13:13:59 +0800

    For testing only(Ignore this)

--- src/fastestdeliveryman/TestDisplayMenu.java
+++ src/fastestdeliveryman/TestDisplayMenu.java
@@ -5,6 +5,8 @@
  */
 package fastestdeliveryman;
 
+import ADT.*;
+
 /**
  *
  * @author ASUS
@@ -15,8 +17,23 @@ public class TestDisplayMenu {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Customer cust = new Customer();
-        cust.login();
+        //Customer cust = new Customer();
+        //cust.login();
+//        LinkedList<AffiliateInterface> a = new LinkedList<>();
+//        a.add(new Affiliate("kappa", "123", "333", "222", "111"));
+//        a.add(new Affiliate("keepo", "123", "resName", "address", "contact"));
+//        System.out.println(a);
+        
+//        LinkedFoodListInterface<Food> m = new LinkedList<>();
+//        m.add(new Food(1, "Cake", 2, 3, 1));
+//        m.add(new Food(2, "jjjj", 3, 1, 2));
+//        m.displayMenuItemWithStatusOrder();
+
+//        LinkedList<Food> a = new LinkedList<>();
+//        
+//        a.add(new Food(1, "Cake", 2, 3, 0));
+//        a.add(new Food(2, "Coke", 3, 1, 1));
+//        System.out.println(a);
     }
     
 }
