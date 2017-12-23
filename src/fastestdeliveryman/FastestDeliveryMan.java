6ad078e2e83bc545a0addd14520d26e64efc670d
LimFangChun <leo477831@gmail.com>
2017-12-23 10:07:07 +0800

    Now affiliate uses sorted linked list with iterator Added a displayClosingMessage() method

--- src/fastestdeliveryman/FastestDeliveryMan.java
+++ src/fastestdeliveryman/FastestDeliveryMan.java
@@ -2,6 +2,7 @@ package fastestdeliveryman;
 
 import java.util.Scanner;
 import ADT.*;
+import java.io.IOException;
 
 /**
  *
@@ -9,14 +10,14 @@ import ADT.*;
  */
 public class FastestDeliveryMan {
 
-    private static final ListWithIteratorInterface<AffiliateInterface> AFFILIATE = new LinkedList<>();
+    private static final SortedListWithIteratorInterface<AffiliateInterface> AFFILIATE = new SortedList();
     private static Object currentUser;
     public static int countAffiliate = 0;
     private static Scanner reader = new Scanner(System.in);
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
-
+        Affiliate.initializeAffiliate(AFFILIATE);
+        
         System.out.println("Welcome to Fastest Delivery Man");
         System.out.println("Please enter a number to select option");
         System.out.println("1. Login as affiliate");
@@ -31,17 +32,16 @@ public class FastestDeliveryMan {
                 currentUser = Affiliate.login(AFFILIATE);
                 break;
             case 2:
-                Affiliate newAffiliate = Affiliate.registerAffiliate();
+                Affiliate newAffiliate = Affiliate.registerAffiliate();           
                 if (newAffiliate != null) {
                     AFFILIATE.add(newAffiliate);
                     currentUser = Affiliate.login(AFFILIATE);
+                    Affiliate.saveAffiliate(AFFILIATE);
                 }
                 break;
             case -1:
             default:
-                System.out.println("=================================");
-                System.out.println("Thank You. Please come back again");
-                System.out.println("=================================");
+                displayClosingMessage();
                 System.exit(0);
         }
 
@@ -52,9 +52,13 @@ public class FastestDeliveryMan {
             System.out.println("Invalid ID or password");
         }
 
-        System.out.println("=================================");
-        System.out.println("Thank You. Please come back again");
-        System.out.println("=================================");
+        displayClosingMessage();
     }
 
+    private static void displayClosingMessage(){
+        System.out.println("===============================================");
+        System.out.println("Thank you for using Fastest Delivery Man System");
+        System.out.println("Please come back again. Goodbye");
+        System.out.println("===============================================");
+    }
 }
