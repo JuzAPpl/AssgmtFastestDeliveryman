148db1e7fe06ef2a0c7faf8fc9e2292b7c6f79bd
JuzAPpl <32742178+JuzAPpl@users.noreply.github.com>
2017-12-25 21:21:20 +0800

    Merge current branch with main

--- src/fastestdeliveryman/Location.java
+++ src/fastestdeliveryman/Location.java
@@ -4,26 +4,31 @@
  * and open the template in the editor.
  */
 package fastestdeliveryman;
+
 import ADT.*;
 import java.util.Scanner;
+
 /**
  *
  * @author User
  */
 public class Location {
-    private int index;
-    private String address;
-    
-    private LinkedQueue<DeliveryMan> availableDelMan = new LinkedQueue<>();
 
-    public Location(int index, String address) {
-        
+    public static ListInterface<Location> map;
+    
+    private int index;
+    private String locationName;
+
+    private ListInterface<Affiliate> restaurants = new LinkedList<>();
+
+    public Location(int index, String locationName) {
+
         this.index = index;
-        this.address = address;
+        this.locationName = locationName;
     }
 
-    private Location() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Location() {
+
     }
 
     public int getIndex() {
@@ -34,54 +39,56 @@ public class Location {
         this.index = index;
     }
 
-    public String getAddress() {
-        return address;
+    public String getLocationName() {
+        return locationName;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+    // ???
+//    public void selectLocation() {
+//        Location local = new Location();
+//
+//        int location;
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("\nAdding The Scheduled Order");
+//        System.out.println("1. Location 1");
+//        System.out.println("2. Location 2");
+//        System.out.println("3. Location 3");
+//        System.out.println("4. Location 4");
+//        System.out.println("5. Location 5");
+//        System.out.println("6. Location 6");
+//        System.out.println("Please select the day(s) you want to make order: ");
+//        location = scan.nextInt();
+//        switch (location) {
+//            case 1:
+//                local.setAddress("Location 1");
+//                local.setIndex(location);
+//                break;
+//            case 2:
+//                local.setAddress("Location 2");
+//                local.setIndex(location);
+//                break;
+//            case 3:
+//                local.setAddress("Location 3");
+//                local.setIndex(location);
+//                break;
+//            case 4:
+//                local.setAddress("Location 4");
+//                local.setIndex(location);
+//                break;
+//            case 5:
+//                local.setAddress("Location 5");
+//                local.setIndex(location);
+//                break;
+//            case 6:
+//                local.setAddress("Location 6");
+//                local.setIndex(location);
+//                break;
+//            default:
+//        }
+//    }
     
-    public void selectLocation(){
-        Location local = new Location();
+    public static void initializeAffiliates(){
+        //TODO: load affiliates from binary file and add them into the map
         
-        int location;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("\nAdding The Scheduled Order");
-        System.out.println("1. Location 1");
-        System.out.println("2. Location 2");
-        System.out.println("3. Location 3");
-        System.out.println("4. Location 4");
-        System.out.println("5. Location 5");
-        System.out.println("6. Location 6");
-        System.out.println("Please select the day(s) you want to make order: ");
-        location = scan.nextInt();
-        switch(location){
-            case 1:
-                local.setAddress("Location 1");
-                local.setIndex(location);
-                break;
-            case 2:
-                local.setAddress("Location 2");
-                local.setIndex(location);
-                break;
-            case 3:
-                local.setAddress("Location 3");
-                local.setIndex(location);
-                break;
-            case 4:
-                local.setAddress("Location 4");
-                local.setIndex(location);
-                break;
-            case 5:
-                local.setAddress("Location 5");
-                local.setIndex(location);
-                break;
-            case 6:
-                local.setAddress("Location 6");
-                local.setIndex(location);
-                break;
-            default:
-        }
     }
 }
