e23817a8896d3f047593c5cbcece4c93289e7e55
JuzAPpl <32742178+JuzAPpl@users.noreply.github.com>
2017-12-26 12:26:08 +0800

    Completed Order class for ah-hoc order

--- src/fastestdeliveryman/Order.java
+++ src/fastestdeliveryman/Order.java
@@ -13,44 +13,43 @@ import java.util.Iterator;
 
 /**
  *
- * @author User
+ * @author Gan Zhen Jie
  */
-public class Order implements Comparable<Order>, Serializable{
+public class Order implements Comparable<Order>, Serializable {
 
     public static final int ADHOC_PENDING = 1;
     public static final int ADHOC_STARTED = 2;
     public static final int ADHOC_COMPLETE = 3;
-    
+
     public static final int SCH_PENDING = 21;
     public static final int SCH_STARTED_INCOMPLETE = 23;
     public static final int SCH_STARTED_COMPLETE = 23;
     public static final int SCH_COMPLETE = 24;
-    
+
     private static int newNum = 0;
     protected String orderNum;
     protected ListWithIteratorInterface<Food> orderedFood;
     protected Date orderDay;
-    protected Location destination; 
+    protected Location destination;
     protected int status;
 
     public Order() {
-        this.orderNum = "S" + String.format("%06d",newNum);
+        this.orderNum = "S" + String.format("%06d", newNum);
         newNum++;
     }
 
-    public Order(Date orderDay, LinkedList<Food> orderedFood, Location destination) {
-        this.orderNum = "S" + String.format("%06d",newNum);
+    public Order(Date orderDay, ListWithIteratorInterface<Food> orderedFood, Location destination, int status) {
+        this.orderNum = "S" + String.format("%06d", newNum);
         this.orderDay = orderDay;
         this.destination = destination;
         this.orderedFood = orderedFood;
-         newNum++;
-    }
-    
-    public static Order addOrder(){
-    return new Order();
+        this.status = status;
+        newNum++;
     }
 
-    
+    public static Order addOrder() {
+        return new Order();
+    }
 
     public Location getDestination() {
         return destination;
@@ -59,8 +58,6 @@ public class Order implements Comparable<Order>, Serializable{
     public void setDestination(Location destination) {
         this.destination = destination;
     }
-
-    
 
     public void setOrderNum(String orderNum) {
         this.orderNum = orderNum;
@@ -83,53 +80,73 @@ public class Order implements Comparable<Order>, Serializable{
     }
 
     public LinkedList<Food> getOrderedFood() {
-        return (LinkedList<Food>)orderedFood;
+        return (LinkedList<Food>) orderedFood;
     }
-  
-    public String toString(){
+
+    public String toString() {
         String orderDetail = "";
-        for(int i=0; i < orderedFood.getNumberOfEntries(); i++)
-        {
+        for (int i = 0; i < orderedFood.getNumberOfEntries(); i++) {
             orderDetail += "";
         }
-        
+
         return orderDetail;
     }
-    
-    public static Order newOrder(LinkedList<Food> foods){
-        //retrieve addr, time, destination from user input
-        
+
+    public static Order newOrder(ListWithIteratorInterface<Food> foods) {
+        ListWithIteratorInterface<Food> orderedFood;
+        Date orderDay;
+        Location destination;
+        int status;
+        //orderedFood = list provided in parameter
+        orderedFood = foods;
+        //retrieve destination from user input(+address & time for scheduled order)
+        destination = Location.getLocation();
         //order date=current time by default
-        
-        
-        //arriveTime =current time+preparatin time+time used to travel from restaurant to destination 
-        
-        return new Order();
+        orderDay = new Date();
+        //status = pending by default(ad-hoc order)
+        status = ADHOC_PENDING;
+        return new Order(orderDay, orderedFood, destination, status);
     }
-    
-    public double getPreparationTime(){
+
+    public double getPreparationTime() {
         //return largest preparation time in ordered food collection
         Iterator it = orderedFood.getIterator();
         double largestPreparationTime = -1;
-        while(it.hasNext()){
-            Food currentFood = (Food)it.next();
+        while (it.hasNext()) {
+            Food currentFood = (Food) it.next();
             double prepTime = currentFood.getPreparationTime();
-            if(prepTime > largestPreparationTime)
+            if (prepTime > largestPreparationTime) {
                 largestPreparationTime = prepTime;
+            }
         }
         return largestPreparationTime;
     }
-    
-    public int getDeliveryTimeTaken(){
+
+    public double getDeliveryTime() {
         //affiliate location vs destination
-        
-        return 0;
+        Affiliate aff = orderedFood.getEntry(1).getFoodOwner();
+        Location temp = aff.getAddress();
+        return destination.getTravelTime(temp);
     }
-    
+
+    public Date getArrivalTime() {
+        Date currentTime = new Date();
+        currentTime.setTime(currentTime.getTime() + (long) (getPreparationTime() * 1000) + (long) (getDeliveryTime() * 1000));
+        return currentTime;
+    }
+
     @Override
-    public int compareTo(Order anotherOrder){
-        //compare order finish time
-        return 0;
-    } 
+    public int compareTo(Order anotherOrder) {
+        //compare order finish time(preparationTime
+        double timeDiff = getPreparationTime() - anotherOrder.getPreparationTime();
+        if (timeDiff > 0) {
+            return 1;
+        } else if (timeDiff < 0) {
+            return -1;
+        } else {
+            return 0;
+        }
+
+    }
 
 }
